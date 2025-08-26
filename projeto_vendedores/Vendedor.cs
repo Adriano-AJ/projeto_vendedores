53ae159d186ae5e39632f10f1e78d13a7c363adc
Admin <Admin@DESKTOP-K73SJPG>
2025-08-26 00:58:13 -0300

    Adicionar arquivos de projeto.

---
+++ projeto_vendedores/Vendedor.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projeto_vendedores
+{
+    internal class Vendedor
+    {
+        private int id;
+        private string nome;
+        private double percComissao;
+        private Venda[] asVendas;
+
+        public int Id { get => id; set => id = value; }
+        public string Nome { get => nome; set => nome = value; }
+        public double PercComissao { get => percComissao; set => percComissao = value; }
+        public Venda[] AsVendas { get => asVendas;}
+
+        public Vendedor(int id, string nome, double percComissao) 
+        {
+            this.id = id;
+            this.nome = nome;
+            this.percComissao = percComissao;
+            this.asVendas = new Venda[31];
+        }
+        public Vendedor() { }
+
+        public void registrarVenda(int dia, Venda venda)
+        {
+            if (dia >= 1 && dia <= 31)
+            {
+                this.asVendas[dia-1] = venda;
+
+            }
+        }
+
+        public double valorVendas()
+        {
+            double valorV = 0;
+
+            for (int i = 0; i < asVendas.Length; i++)
+            {
+                valorV += asVendas[i].valorMedio();
+            }
+            
+            return valorV;
+        }
+        public double valorComissao()
+        {
+            return valorVendas() * (percComissao / 100); ;
+        }
+
+    }
+}
---
+++ projeto_vendedores/Vendedores.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projeto_vendedores
+{
+    internal class Vendedores
+    {
+        private Vendedor[] osVendedores;
+        private int max;
+        private int qtde;
+
+        public int Max { get => max;}
+        public int Qtde { get => qtde;}
+        public Vendedor[] OsVendedores { get => osVendedores;}
+
+        public Vendedores(int max)
+        {
+            this.max = max;
+            this.qtde = 0;
+            this.osVendedores = new Vendedor[max];
+        }
+
+        public bool adicionar(Vendedor v)
+        {
+            bool podeAdicionar = (this.qtde < this.max);
+            if (podeAdicionar)
+                this.osVendedores[this.qtde++] = v;
+            return podeAdicionar;
+        }
+
+        public Vendedor pesquisar(Vendedor v)
+        {
+            Vendedor vendedorAchado = new Vendedor();
+            foreach (Vendedor vendedor in this.osVendedores)
+            {
+                if (vendedor.Equals(v))
+                {
+                    vendedorAchado = vendedor;
+                    break;
+                }
+            }
+            /*
+            int i = 0;
+            while (i < this.max && this.asCoisas[i].Id != c.Id)
+            {
+                ++i;
+            }
+            if (i < this.max)
+            {
+                coisaAchada = this.asCoisas[i];
+            }
+            */
+            return vendedorAchado;
+        }
+
+        public bool remover(Vendedor v)
+        {
+            int j;
+            bool podeRemover = (pesquisar(v).Id != -1);
+            if (podeRemover)
+            {
+                int i = 0;
+                while (i < this.max && this.osVendedores[i].Id != v.Id)
+                {
+                    ++i;
+                }
+                for (j = i; j < this.max - 1; ++j)
+                {
+                    this.osVendedores[j] = this.osVendedores[j + 1];
+                }
+                this.osVendedores[j] = new Vendedor();
+                this.qtde--;
+            }
+            return podeRemover;
+        }
+    }
+}
